f25a978f24c3bbf4fef4417853d0944de559831e
水原樹 <>
2026-02-15 01:30:34 +0900

    first commit

---
+++ ShazamKit_Practice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ShazamKit_Practice
+//
+//  Created by 水原樹 on 2026/02/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ShazamKit_Practice/ShazamKit_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShazamKit_PracticeApp.swift
+//  ShazamKit_Practice
+//
+//  Created by 水原樹 on 2026/02/15.
+//
+
+import SwiftUI
+
+@main
+struct ShazamKit_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
